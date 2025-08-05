a4943ee00deaabe898f347387ce6d761e77cd85c
Renan Queiroz Chaves <115646713+Renan-pien@users.noreply.github.com>
2025-08-05 01:26:41 +0000

    Adiciona script principal para entrada de nome e cor de cabelo

---
+++ main.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome: '))
+cabelo = str(input('Digite a cor do seu cabelo: '))
+
+
+if nome == 'wesley' and cabelo == 'loirin':
+    print('voce é gay')
+else:
+    print('voce é homem')    
